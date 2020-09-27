b5d6a7cafbdbbe9ac2a7be7dfd51a4e81d7566ac
Sykkoss <damien.cosson@epitech.eu>
2020-09-27 14:38:27 +0200

    add: Client and Animal stats classes

---
+++ Assets/Scripts/Stats/AnimalStats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalStats : MonoBehaviour
+{
+    public HouseType HouseType { get; private set; }
+    public bool HasGarden { get; private set; }
+    public bool HasChildren { get; private set; }
+    public bool HasOtherAnimals { get; private set; }
+    public int Cost { get; private set; }
+    public AnimalPersonality AnimalPersonality { get; private set; }
+
+
+    public AnimalStats(HouseType houseType, bool hasGarden, bool hasChildren,
+        bool hasOtherAnimals, int cost, AnimalPersonality animalPersonality)
+    {
+        HouseType = houseType;
+        HasGarden = hasGarden;
+        HasChildren = hasChildren;
+        HasOtherAnimals = hasOtherAnimals;
+        Cost = cost;
+        AnimalPersonality = animalPersonality;
+    }
+
+    public AnimalStats()
+    {
+        // Random every stat
+    }
+}
---
+++ Assets/Scripts/Stats/ClientStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientStats : MonoBehaviour
+{
+    public HouseType HouseType { get; private set; }
+    public bool HasGarden { get; private set; }
+    public bool HasChildren { get; private set; }
+    public bool HasOtherAnimals { get; private set; }
+    public int Income { get; private set; }
+    public ClientPersonality ClientPersonality { get; private set; }
+    public AnimalPersonality AnimalPersonalityWanted { get; private set; }
+
+
+    public ClientStats(HouseType houseType, bool hasGarden, bool hasChildren, bool hasOtherAnimals, int income,
+        ClientPersonality clientPersonality, AnimalPersonality animalPersonalityWanted)
+    {
+        HouseType = houseType;
+        HasGarden = hasGarden;
+        HasChildren = hasChildren;
+        HasOtherAnimals = hasOtherAnimals;
+        Income = income;
+        ClientPersonality = clientPersonality;
+        AnimalPersonalityWanted = animalPersonalityWanted;
+    }
+
+    public ClientStats()
+    {
+        // Random every stat
+    }
+}
