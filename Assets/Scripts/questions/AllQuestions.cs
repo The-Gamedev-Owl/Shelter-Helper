6c8afb83ea88f2c04ab2ad8e6f5d5d4e621b5234
Kahla <Kahla@KAHLAN-PC.local>
2020-09-28 22:16:00 +0200

    Start of question structure

---
+++ Assets/Scripts/questions/AllQuestions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AllQuestions", menuName = "AllQuestions")]
+public class AllQuestions : ScriptableObject
+{
+    public List<Question> allQuestions;
+}
